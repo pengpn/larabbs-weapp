4339f43c499dc226c393e95fc39b63f75fe2a3c9
pnpeng <iopkq999@gmail.com>
2019-07-05 17:53:50 +0800

    update avatar

--- src/utils/api.js
+++ src/utils/api.js
@@ -144,11 +144,35 @@ const logout = async (params = {}) => {
   return logoutResponse
 }
 
+const updateFile = async (options = {}) => {
+  // 显示loading
+  wepy.showLoading({title: '上传中'})
+
+  // 获取 token
+  let accessToken = await getToken()
+
+  // 拼接url
+  options.url = host + '/' + options.url
+  let header = options.header || {}
+  // 将token设置在header中
+  header.Authorization = 'Bearer ' + accessToken
+  options.header = header
+
+  // 上传文件
+  let response = await wepy.uploadFile(options)
+
+  // 隐藏 loading
+  wepy.hideLoading()
+
+  return response
+}
+
 export default {
   request,
   authRequest,
   refreshToken,
   getToken,
   login,
-  logout
+  logout,
+  updateFile
 }
